64bbf115587a32109b315c0913ead9f7db7f95f7
Cyrille G. <pro@cyrilleg.me>
2026-03-31 15:20:46 +0200

    fix typo in the tag interface

--- pkg/services/library/tag.go
+++ pkg/services/library/tag.go
@@ -12,16 +12,16 @@ type Taggable interface {
 
 type TagService interface {
 
-	// Add adds a tag to the SR.
+	// Add adds a tag to the resource.
 	// Parameters:
-	//   - id: ID of the SR
+	//   - id: ID of the resource to which the tag will be added
 	//   - tag: tag string to add
 	// Returns an error if the operation fails.
 	Add(ctx context.Context, id uuid.UUID, tag string) error
 
-	// Remove removes a tag from the SR.
+	// Remove removes a tag from the resource.
 	// Parameters:
-	//   - id: ID of the SR
+	//   - id: ID of the resource from which the tag will be removed
 	//   - tag: tag string to remove
 	// Returns an error if the operation fails.
 	Remove(ctx context.Context, id uuid.UUID, tag string) error
